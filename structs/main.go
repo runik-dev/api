45694c7ef266970e8a8472c1b0846f5daad77a25
Ultraviolet <ultraviolet-hq@tuta.io>
2023-12-17 11:53:26 +0000

    Update main.go

--- structs/main.go
+++ structs/main.go
@@ -17,9 +17,13 @@ type Environment struct {
 	Port             string
 	Rps              string
 	StorageBucket    string
+	GitToken         string
+	GitUrl           string
+	GitUsername      string
+	GitTemplate      string
 }
 type User struct {
-	ID        string `gorm:"uniqueIndex"`
+	ID        string `gorm:"type:bigint;primaryKey"`
 	Email     string `gorm:"uniqueIndex"`
 	Password  string `gorm:"notNull"`
 	Verified  bool   `gorm:"default:false"`
@@ -30,6 +34,14 @@ type ApiUser struct {
 	ID       string `json:"id"`
 	Email    string `json:"email"`
 	Verified bool   `json:"verified"`
+}
+type Project struct {
+	ID        string `gorm:"uniqueIndex"`
+	UserID    string `gorm:"type:bigint"`
+	User      User   `gorm:"foreignKey:UserID"`
+	Name      string `gorm:"notNull"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 type Session struct {
 	UserID string
