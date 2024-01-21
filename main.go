52878bec5abe28702684adf7082c0202f4e60eea
Ultraviolet <ultraviolet-hq@tuta.io>
2024-01-21 11:32:45 +0000

    Update main.go

--- main.go
+++ main.go
@@ -83,7 +83,7 @@ func main() {
 	app := fiber.New(fiber.Config{
 		JSONEncoder: sonic.Marshal,
 		JSONDecoder: sonic.Unmarshal,
-		Prefork:     true,
+		Prefork:     false,
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
 			return c.Status(fiber.StatusBadRequest).JSON(routes.GlobalErrorHandlerResp{
 				Success: false,
