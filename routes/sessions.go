0199aae9fa7bddf480faa4298a5d5c70100ab42a
Ultraviolet <ultraviolet-hq@tuta.io>
2024-01-21 09:38:05 +0000

    Update sessions.go

--- routes/sessions.go
+++ routes/sessions.go
@@ -12,6 +12,7 @@ import (
 	"github.com/bytedance/sonic"
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/pquerna/otp/totp"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -22,11 +23,11 @@ type PostSessions struct {
 	IP       *string `json:"ip" validate:"omitempty,ip"`
 }
 
-func getExpiration(body PostSessions) time.Duration {
-	if body.Expire {
+func getExpiration(expires bool) time.Duration {
+	if expires {
 		return time.Hour * 24 * 10
 	} else {
-		return -1
+		return time.Hour * 24 * 30 * 6
 	}
 }
 func getIp(body PostSessions, c *fiber.Ctx) string {
@@ -48,21 +49,25 @@ func postSessions(c *fiber.Ctx) error {
 	}
 	errs := _validate.Validate(body)
 	if err, rtrn := handleValidateErrors(errs, c); rtrn {
-
 		return err
 	}
 
 	var user structs.User
 	if err := db.Where(&structs.User{Email: body.Email}).First(&user).Error; err != nil {
 		fmt.Println("User not found")
-		return c.Status(http.StatusBadRequest).JSON(errors.UserCredentialsInvalid)
+		return c.Status(http.StatusNotFound).JSON(errors.UserCredentialsInvalid)
 	}
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password)); err != nil {
 		fmt.Println("password wrong")
-		return c.Status(http.StatusBadRequest).JSON(errors.UserCredentialsInvalid)
+		return c.Status(http.StatusUnauthorized).JSON(errors.UserCredentialsInvalid)
+	}
+	if user.TotpVerified {
+		id := generator.Generate()
+		rdb.Set(ctx, "totp:"+id.String(), user.ID, time.Minute * 15)
+		return c.Status(http.StatusOK).JSON(fiber.Map{"action_name": "totp", "totp_id":id.String()})
 	}
 	ip := getIp(body, c)
-	expiration := getExpiration(body)
+	expiration := getExpiration(body.Expire)
 	token, err := utils.RandString(32)
 	if err != nil {
 		return c.Status(http.StatusBadRequest).JSON(errors.ServerTokenGenerate)
@@ -73,6 +78,59 @@ func postSessions(c *fiber.Ctx) error {
 		return c.Status(http.StatusInternalServerError).JSON(errors.ServerStringifyError)
 	}
 	rdb.Set(ctx, "session:"+token, stringified, expiration)
+	return c.JSON(fiber.Map{"token": token})
+}
+func confirm2faSignIn(c *fiber.Ctx) error {
+	totpId := c.Params("totp")
+	if totpId== "" {
+		return c.Status(http.StatusBadRequest).JSON(errors.MissingParameter)
+	}
+
+	expireStr := c.Query("expire")
+	var expire bool
+	if expireStr == "true" {
+		expire = true
+	}
+
+	authorization := c.Get("Authorization")
+	if authorization != env.GlobalAuth {
+		return c.Status(http.StatusUnauthorized).JSON(errors.AuthorizationInvalid)
+	}
+
+	code := c.Query("code")
+	if code == "" {
+		return c.Status(http.StatusBadRequest).JSON(errors.MissingParameter)
+	}
+	
+	val, err := rdb.Get(ctx, "totp:"+totpId).Result()
+	if err == redis.Nil {
+		return c.Status(http.StatusUnauthorized).JSON(errors.NotFound)
+	} else if err != nil {
+		return c.Status(500).JSON(errors.ServerSqlError)
+	}
+	var user structs.User
+	if err := db.Where(&structs.User{ID: val}).First(&user).Error; err != nil {
+		fmt.Println("User not found")
+		return c.Status(http.StatusNotFound).JSON(errors.UserCredentialsInvalid)
+	}
+	valid := totp.Validate(code, user.TotpSecret)
+	if !valid {
+		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"code": "invalid_totp"})
+	}
+
+	ip := c.IP() // add IP optionin body at somepoint
+	expiration := getExpiration(expire)
+	token, err := utils.RandString(32)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(errors.ServerTokenGenerate)
+	}
+	stringified, err := sonic.Marshal(structs.Session{UserID: user.ID, IP: ip})
+	if err != nil {
+		fmt.Println(err.Error())
+		return c.Status(http.StatusInternalServerError).JSON(errors.ServerStringifyError)
+	}
+	rdb.Set(ctx, "session:"+token, stringified, expiration)
+	rdb.Del(ctx, "totp:"+totpId)
 	return c.JSON(fiber.Map{"token": token})
 }
 func getSessionIps(c *fiber.Ctx, userId string) ([]string, error) {
